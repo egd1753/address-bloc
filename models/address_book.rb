6a4cd716848a17111f44e113a68f04fb3f0c3cdd
egd1753 <egd1753@gmail.com>
2019-05-16 15:30:56 -0500

    completed assignment 3 models work

--- models/address_book.rb
+++ models/address_book.rb
@@ -17,4 +17,13 @@
          end
          entries.insert(index, Entry.new(name, phone_number, email))
        end
+
+       def remove_entry(name, phone_number, email)
+         entries.each do |entry|
+           if entry.name == name
+             entries.delete_at(entries.index(entry))
+           end
+         end 
+       end
+
    end
