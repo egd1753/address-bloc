29a398cf5f7b35613d4667a3d26a0a00a4487841
egd1753 <egd1753@gmail.com>
2019-05-18 18:49:09 -0500

    completed checkpoint 6 searching work

--- models/address_book.rb
+++ models/address_book.rb
@@ -54,13 +54,5 @@
          return nil
        end
 
-       def iterative_search(name)
-         entries.each do |entry|
-           if (entry.name == name)
-             return entry
-           end
-         end
-         return nil
-       end
 
    end
